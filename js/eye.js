9be6f40596b247bd9f7a3382ad293bcdd820fd27
liza-rgb <68602250+liza-rgb@users.noreply.github.com>
2022-07-22 17:27:54 +0200

    Added movement of the eye on the logo

---
+++ js/eye.js
@@ -0,0 +1,15 @@
+let body = document.querySelector("body");
+let eye = document.querySelector(".eye");
+
+body.addEventListener('mousemove', (event) => {
+    let mouseX = eye.getBoundingClientRect().left;
+    let mouseY = eye.getBoundingClientRect().top;
+
+    mouseX = event.clientX - mouseX;
+    mouseY = event.clientY - mouseY;
+
+    let radians = Math.atan2(mouseX, mouseY);
+    let rotationDegrees = -1 * radians * (180 / Math.PI) + 180;
+
+    eye.style.transform = `rotate(${rotationDegrees}deg)`;
+});
